452a1d842becb31167fb28aca008cddcbffa4e6f
Repikas <cristianf2018@gmail.com>
2022-10-17 15:17:22 -0300

    Cambios hasta el momento del proyecto coderhouse

---
+++ Python/ejercicio_1.py
@@ -0,0 +1,15 @@
+def mas_breves(cad,tup):
+    cad2=[]
+    maximo=len(cad)
+    maximo_tupla=len(tup)
+    for i in len(tup):
+        if int[i] <= maximo:
+            cad2.append(tup[i])
+    return cad2
+
+
+print("Hola mundo")
+
+
+
+
